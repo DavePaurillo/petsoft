eb8b2d9618dc03e42b2fbd4abd951f1e63f4ca39
DavePaurillo <paurillo.dave@gmail.com>
2024-12-26 09:53:07 +0800

    feat: Home page | Shadcn initialization

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
+import logo from "../../../public/logo.svg";
+
 export default function Home() {
-	return <main>Home</main>
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-10 bg-[#5dc9a8] xl:flex-row">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
+        alt="Petsoft Preview"
+        height={519}
+        width={472}
+      />
+
+      <div>
+        <Link href="/">
+          <Image src={logo} alt="Petsoft Logo" />
+        </Link>
+        <h1 className="my-6 max-w-[500px] text-5xl font-semibold">
+          Manage your <span className="font-extrabold">pet daycare</span> with
+          ease
+        </h1>
+        <p className="max-w-[600px] text-2xl font-medium">
+          Use PetSoft to easily keep track of pets under your care. Get lifetime
+          access for $299.
+        </p>
+        <div className="mt-10 space-x-3">
+          <Button asChild>
+            <Link href="/signup">Get Started</Link>
+          </Button>
+          <Button variant="secondary" asChild>
+            <Link href="/login">Log in</Link>
+          </Button>
+        </div>
+      </div>
+    </main>
+  );
 }
---
+++ src/lib/utils.ts
@@ -0,0 +1,8 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+// dark:[^ "]+  --  hack to get dark mode classes
