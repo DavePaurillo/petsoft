b8c3dc841d7ee8b1b968941e89323cd43cd1e5bf
DavePaurillo <paurillo.dave@gmail.com>
2024-12-26 19:06:58 +0800

    style: AppFooter

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function AppFooter() {
-  return <div>AppFooter</div>;
+  return (
+    <footer className="mt-auto border-t border-black/5 py-5 ">
+      <small className="opacity-50">
+        &copy; 2030 PetSoft. All rights reserved.
+      </small>
+    </footer>
+  );
 }
